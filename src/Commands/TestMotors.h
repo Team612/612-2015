459bab6c3309bda78731c33e2246ac76084caebf
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2015-01-17 14:40:47 -0500

    Test Motor command finished

---
+++ src/Commands/TestMotors.h
@@ -0,0 +1,25 @@
+#ifndef TestMotors_H
+#define TestMotors_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+#include "../Subsystems/MecanumDrivetrain.h"
+#include <Timer.h>
+#include "../ports.h"
+
+class TestMotors: public CommandBase
+{
+private:
+	Timer* testMotorTimer;
+	MecanumDrivetrain* drive;
+	bool isTestDone;
+public:
+	TestMotors();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif
