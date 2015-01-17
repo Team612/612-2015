e832fc8e372eed4d61bcabe9d06762a4974ab81c
ZackAlfakir <zalfakir@yahoo.com>
2015-01-17 15:50:23 -0500

    Revert "changed mecanum drive stuff"
    This reverts commit 3a8d850e5852aff80251bc861a336d4a549aeba2.

--- Simulate/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lWPILibSim -lgazebo -lgazebo_transport -lgazebo_msgs -lgazebo_common -lprotobuf -lboost_system -ldl -lpthread
-
