8bfbffffae3525d275834c94eb5af77d5c572d7c
toastertaster <toastertaster@gmail.com>
2015-01-20 22:49:54 -0500

    Some comments to help steer the code development

--- src/Subsystems/Latch.cpp
+++ src/Subsystems/Latch.cpp
@@ -3,16 +3,19 @@
 
 Latch::Latch()
 {
-
+    // Initialize the variable you made in Latch.h
 }
 
 void Latch::Open()
 {
-
+    // Using the WPILIB documentation, you would use the variable
+    // and one of its methods to enable the latch.
 
 }
 
 void Latch::Close()
 {
+    // Same as the previous, except you disable the latch.
 
 }
+
--- src/Subsystems/Latch.h
+++ src/Subsystems/Latch.h
@@ -5,6 +5,11 @@
 
 class Latch: public Subsystem
 {
+    // I think you're going to need a private variable.
+    // The variable type would be either a Relay or Solenoid
+    // see https://chantilly612code.github.io/612-2015/classRelay.html
+    // see https://chantilly612code.github.io/612-2015/classSolenoid.html
+
 public:
 	Latch();
 	void Open();
