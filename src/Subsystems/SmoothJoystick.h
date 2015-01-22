1d6aa6e4fdad869a02c18e2edd74094cc5184058
Shaun Wu <Alexbay218@gmail.com>
2015-01-22 16:04:57 -0500

    Add smoothjoystick. please check for errors. (In SmoothJoystick.cpp) GetY() and GetX() show errors. So does joystick = new Joystick(1);. . . . GetValue() can be used to get value of an joystick (Run InitJoystick() first). X axis = 0, y axis = 1 left hand = 0, right hand = 1

---
+++ src/Subsystems/SmoothJoystick.h
@@ -0,0 +1,27 @@
+#ifndef MecanumDrivetrain_H
+#define MecanumDrivetrain_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+#include <Joystick.h>
+
+class SmoothJoystick: public Subsystem, public Joystick
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	Joystick *joystick;
+
+public:
+	SmoothJoystick(); //Constructor
+	void InitDefaultCommand();
+	void InitJoystick();
+	float GetValue(int JoystickHand, int Axis);
+	/*
+	 * GetValue() can be used to get value of an joystick (Run InitJoystick() first).
+	 * X axis = 0, y axis = 1
+	 * left hand = 0, right hand = 1
+	 */
+};
+
+#endif
