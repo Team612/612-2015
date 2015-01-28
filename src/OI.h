6bc11bb1fcbe947081aa793f020abe9b0a6ea5c7
Shaun Wu <Alexbay218@gmail.com>
2015-01-28 15:48:57 -0500

    Add OI Stuff. Only added joysticks and buttons.

--- src/OI.h
+++ src/OI.h
@@ -6,9 +6,18 @@
 class OI
 {
 private:
+		Joystick *right_joystick;
+		Joystick *left_joystick;
+
+		JoystickButton *button_a;
+		JoystickButton *button_b;
+		JoystickButton *button_x;
+		JoystickButton *button_y;
+
 
 public:
 	OI();
+
 };
 
 #endif
