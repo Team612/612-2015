0f0c7bce5d89e111a68213bbd631247604f63c6d
ChampD <AMOTHWO@gmail.com>
2015-01-28 16:07:52 -0500

    Merged the Merge branch into master, Fixed compile errors in master, best commit.

---
+++ src/Commands/Drive.cpp
@@ -0,0 +1,39 @@
+#include "../CommandBase.h"
+#include "Drive.h"
+
+Drive::Drive()
+{
+	// Use Requirevas() here to declare subsystem dependencies
+	// eg. Requires(chassis)
+	Requires(drivetrain);
+}
+// Called just before this Command runs the first time
+void Drive::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void Drive::Execute(float inMagnitude, float inDirection, float inRotation)
+{
+	drivetrain->move(inMagnitude, inDirection, inRotation);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool Drive::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void Drive::End()
+{
+	drivetrain->stop();
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void Drive::Interrupted()
+{
+	drivetrain->stop();
+}
