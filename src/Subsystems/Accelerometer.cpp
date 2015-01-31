c2313c601ab1e8c2de72592772cdf791c79d5c94
Benjamin Ward <ward.programm3r@gmail.com>
2015-01-31 11:08:07 -0500

    Add accelerometer template

--- src/Subsystems/Accelerometer.cpp
+++ src/Subsystems/Accelerometer.cpp
@@ -1,10 +1,49 @@
-/*
- * Accelerometer.cpp
- *
- *  Created on: Jan 31, 2015
- *      Author: user
- */
+#include "Accelerometer.h"
 
+Accelerometer::Accelerometer() :
+	Subsystem("Accelerometer")
+{
+	talon = new Talon(Accelerometer_MOTOR);
+	topSwitch = new DigitalInput(Accelerometer_TOP_SWITCH);
+	bottomSwitch = new DigitalInput(Accelerometer_BOTTOM_SWITCH);
+	encoder = new Encoder(Accelerometer_ENCODER_A, Accelerometer_ENCODER_B);
+}
 
+Accelerometer::~Accelerometer()
+{
+	delete talon;
+}
 
+void Accelerometer::InitDefaultCommand()
+{
+	//SetDefaultCommand(new AccelerometerUp());
+}
+
+void Accelerometer::move(float magnitude)
+{
+	//Checks the sensors to see if the Accelerometer is at the top or the bottom
+	bool topInput = topSwitch->Get();
+	bool bottomInput = bottomSwitch->Get();
+
+	//If the sensors give any input then the motors are set to a speed of 0
+	if (topInput || bottomInput)
+	{
+		talon->Set(0);
+	}
+	else
+	{
+		talon->Set(magnitude);
+	}
+}
+
+void Accelerometer::stop()
+{
+	//Sets motor speed to nothing
+	talon->Set(0);
+}
+
+Encoder* Accelerometer::getEncoder()
+{
+	return encoder;
+}
 
--- src/Subsystems/Accelerometer.h
+++ src/Subsystems/Accelerometer.h
@@ -1,15 +1,18 @@
-/*
- * Accelerometer.h
- *
- *  Created on: Jan 31, 2015
- *      Author: user
- */
-
 #ifndef SRC_SUBSYSTEMS_ACCELEROMETER_H_
 #define SRC_SUBSYSTEMS_ACCELEROMETER_H_
 
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+#include "../RobotMap.h"
 
-
-
+class Accelerometer: public Subsystem {
+private:
+	BuiltInAccelerometer accel;
+public:
+	Accelerometer();
+	virtual ~Accelerometer();
+	void InitDefaultCommand();
+	// Other methods here (getValues, etc)
+};
 
 #endif /* SRC_SUBSYSTEMS_ACCELEROMETER_H_ */
