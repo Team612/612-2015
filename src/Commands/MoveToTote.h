5e70155cd09f2bde8dd1bed2cf58094c80021d4f
Benjamin Ward <ward.programm3r@gmail.com>
2015-01-30 22:01:02 -0500

    Adding template classes for use in the command group

--- src/Commands/MoveToTote.h
+++ src/Commands/MoveToTote.h
@@ -1,15 +1,18 @@
-/*
- * MoveToTote.h
- *
- *  Created on: Jan 30, 2015
- *      Author: user
- */
-
 #ifndef SRC_COMMANDS_MOVETOTOTE_H_
 #define SRC_COMMANDS_MOVETOTOTE_H_
 
+#include "../CommandBase.h"
+#include "WPILib.h"
 
-
-
+class MoveToTote: public CommandBase
+{
+public:
+	MoveToTote();
+	void Initialize(); ///< Runs after the constructor
+	void Execute(); ///< This method is called repeatedly
+	bool IsFinished(); ///< This method contains the logic of when the command should end
+	void End(); ///< The clean-up method when a command ends gracefully (IsFinished returns true)
+	void Interrupted(); ///< The clean-up method when a command ends abruptly (from another event, such as a button release)
+};
 
 #endif /* SRC_COMMANDS_MOVETOTOTE_H_ */
