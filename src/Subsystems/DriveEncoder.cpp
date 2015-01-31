506b4e007db678cba9710f80be41369f045dbb37
Shaun Wu <Alexbay218@gmail.com>
2015-01-31 15:35:33 -0500

    Add documentation and rename stuff. 01.31.15

--- src/Subsystems/DriveEncoder.cpp
+++ src/Subsystems/DriveEncoder.cpp
@@ -4,33 +4,33 @@
 DriveEncoder::DriveEncoder() :
 		Subsystem("Encoder")
 {
-	encoders[0] = new Encoder(ENCODER_LR_A, ENCODER_LR_B);
-	encoders[1] = new Encoder(ENCODER_LF_A, ENCODER_LF_B);
-	encoders[2] = new Encoder(ENCODER_RR_A, ENCODER_RR_B);
-	encoders[3] = new Encoder(ENCODER_RF_A, ENCODER_RF_B);
+	encoders[0] = new Encoder(ENCODER_LR_A, ENCODER_LR_B); //The rear left encoder
+	encoders[1] = new Encoder(ENCODER_LF_A, ENCODER_LF_B); //The front left encoder
+	encoders[2] = new Encoder(ENCODER_RR_A, ENCODER_RR_B); //The rear right encoder
+	encoders[3] = new Encoder(ENCODER_RF_A, ENCODER_RF_B); //The front right encoder
 }
 
 DriveEncoder::~DriveEncoder()
 {
-	delete encoders;
+	delete encoders; //deletes encoders for more resources
 }
 
-int32_t DriveEncoder::getSpins(uint32_t encoder)
+int32_t DriveEncoder::GetEncoderSpins(uint32_t encoder) //returns the spin from the encoder you want
 {
 	return encoders[encoder]->Get();
 }
 
-double DriveEncoder::getDistance(uint32_t encoder)
+double DriveEncoder::GetEncoderDistance(uint32_t encoder) //returns the spin from the encoder you want
 {
 	return encoders[encoder]->GetDistance();
 }
 
-double DriveEncoder::getRate(uint32_t encoder)
+double DriveEncoder::GetEncoderRate(uint32_t encoder) //returns the spin from the encoder you want
 {
 	return encoders[encoder]->GetRate();
 }
 
-void DriveEncoder::reset()
+void DriveEncoder::ResetAllEncoders() //reset the distance total on all encoders
 {
 	for (int index = 0; index < 4; index++)
 	{
@@ -39,7 +39,7 @@ void DriveEncoder::reset()
 	}
 }
 
-void DriveEncoder::reset(uint32_t encoder)
+void DriveEncoder::ResetEncoder(uint32_t encoder) //reset the distance total on the encoder you want
 {
 	printf("Resetting drive encoder #%u", encoder);
 	encoders[encoder]->Reset();
--- src/Subsystems/DriveEncoder.h
+++ src/Subsystems/DriveEncoder.h
@@ -7,13 +7,13 @@
 class DriveEncoder: public Subsystem
 {
 private:
-	Encoder* encoders[4];
+	Encoder* encoders[4]; //A encoder array, four members for the drivetrain
 public:
-	DriveEncoder();
-	virtual ~DriveEncoder();
+	DriveEncoder(); //
+	virtual ~DriveEncoder(); //deconstructor
 	void InitDefaultCommand();
 
-	int32_t getSpins(uint32_t);
+	int32_t GetEncoderSpins(uint32_t);
 	/*Gets the spins of the corresponding motor
 	 * 0 = Left-Rear Motor
 	 * 1 = Left-Front Motor
@@ -21,16 +21,16 @@ public:
 	 * 3 = Right-Front Motor
 	 */
 
-	double getDistance(uint32_t);
+	double GetEncoderDistance(uint32_t);
 	//Gets distance since the last reset of the corresponding encoder (see list above)
 
-	double getRate(uint32_t);
+	double GetEncoderRate(uint32_t);
 	//Gets rate of the corresponding encoder (see list above)
 
-	void reset();
+	void ResetAllEncoders();
 	//Resets all encoders
 
-	void reset(uint32_t);
+	void ResetEncoder(uint32_t);
 	//Resets the corresponding encoder
 
 };
