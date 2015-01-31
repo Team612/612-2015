e9c1a4803686ce1ec87d807a977ed9cba7a9c428
Benjamin Ward <ward.programm3r@gmail.com>
2015-01-30 21:58:37 -0500

    Adding empty header and source files

---
+++ src/Commands/MoveToTote.cpp
@@ -0,0 +1,10 @@
+/*
+ * MoveToTote.cpp
+ *
+ *  Created on: Jan 30, 2015
+ *      Author: user
+ */
+
+
+
+
---
+++ src/Commands/MoveToTote.h
@@ -0,0 +1,15 @@
+/*
+ * MoveToTote.h
+ *
+ *  Created on: Jan 30, 2015
+ *      Author: user
+ */
+
+#ifndef SRC_COMMANDS_MOVETOTOTE_H_
+#define SRC_COMMANDS_MOVETOTOTE_H_
+
+
+
+
+
+#endif /* SRC_COMMANDS_MOVETOTOTE_H_ */
