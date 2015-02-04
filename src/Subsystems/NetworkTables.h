e3c0d8ea0ce316a23ae6952094d1a9f797651eb3
Benjamin Ward <ward.programm3r@gmail.com>
2015-02-04 15:45:22 -0500

    NetworkTables template complete

--- src/Subsystems/NetworkTables.h
+++ src/Subsystems/NetworkTables.h
@@ -1,23 +1,16 @@
-#ifndef SRC_SUBSYSTEMS_ELEVATOR_H_
-#define SRC_SUBSYSTEMS_ELEVATOR_H_
+#ifndef SRC_SUBSYSTEMS_NETWORK_TABLES_H_
+#define SRC_SUBSYSTEMS_NETWORK_TABLES_H_
 
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 #include "../RobotMap.h"
 
-class Elevator : public Subsystem {
+class NetworkTables : public Subsystem {
 private:
-	Talon* talon;
-	DigitalInput* topSwitch;
-	DigitalInput* bottomSwitch;
-	Encoder* encoder;
 public:
-	Elevator();
-	virtual ~Elevator();
-	void move(float magnitude);
-	void stop();
+	NetworkTables();
+	virtual ~NetworkTables();
 	void InitDefaultCommand();
-	Encoder* getEncoder();
 };
 
-#endif /* SRC_SUBSYSTEMS_ELEVATOR_H_ */
+#endif /* SRC_SUBSYSTEMS_NETWORK_TABLES_H_ */
