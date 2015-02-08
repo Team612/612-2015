0a9e06a6629c4e5d1798da855c892fd80526bd11
ZackAlfakir <zalfakir@yahoo.com>
2015-02-07 20:40:24 -0500

    Small mistakes that dont get caught

--- compiler-download.sh
+++ compiler-download.sh
@@ -2,5 +2,5 @@
 
 # Will download required packages for building
 sudo apt-add-repository -y ppa:byteit101/frc-toolchain 
-sudo apt update -y
-sudo apt install -y frc-toolchain
+sudo apt-get update -y
+sudo apt-get install -y frc-toolchain
