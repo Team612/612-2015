9ad6547ace56a5e7b2134037d6a2efad9baf5b09
ZackAlfakir <zalfakir@yahoo.com>
2015-02-08 01:25:18 -0500

    We need to delete the generated cmake files in order to keep Eclipse from throwing compile errors in the project

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
 cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=./arm.cmake robot.cmake ..
 make VERBOSE=1
 
+#delete files that will make Eclipse fail
+echo "Removing Cmake files!!"
+rm -rf CMakeFiles
