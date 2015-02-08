ed8ae7c2c5278727fccffd3d74417bffcc081528
ZackAlfakir <zalfakir@yahoo.com>
2015-02-07 22:53:02 -0500

    Running build.sh will build it on a local system too. We can compile from terminal

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#satisfy dependencies
+./.wpilib-download
+./.compiler-download
+
+#build
 cd ./.build
 rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
 cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=./arm.cmake robot.cmake ..
