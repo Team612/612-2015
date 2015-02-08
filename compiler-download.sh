063f65590fcb1daa581f022bcee9301ce796e844
ZackAlfakir <zalfakir@yahoo.com>
2015-02-07 20:38:14 -0500

    Fixed hangup

--- compiler-download.sh
+++ compiler-download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Will download required packages for building
-sudo apt-add-repository ppa:byteit101/frc-toolchain 
-sudo apt update 
-sudo apt install frc-toolchain
+sudo apt-add-repository -y ppa:byteit101/frc-toolchain 
+sudo apt update -y
+sudo apt install -y frc-toolchain
