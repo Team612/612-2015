81a0c59c5bd9039faa1c208ef59d184b6fb96ed9
ZackAlfakir <zalfakir@yahoo.com>
2015-02-07 22:32:27 -0500

    Broke it again

--- .compiler-download.sh
+++ .compiler-download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Will download required packages for building
-dpkg -sq frc-toolchain
+dpkg-query -s frc-toolchain
 if [ $? = 1 ]; then
 	sudo apt-add-repository -y ppa:byteit101/frc-toolchain 
 	sudo apt-get update -y
