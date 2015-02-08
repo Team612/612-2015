4a98621e504af6c538c6fcf746e151053e85d18b
ZackAlfakir <zalfakir@yahoo.com>
2015-02-07 22:03:35 -0500

    Changed some files to hidden

--- .wpilib-download.sh
+++ .wpilib-download.sh
@@ -9,14 +9,22 @@
 
 # TODO: script determining the latest .jar file based on the date string
 # rather than a hard coded file name.
+version="0.1.0.201501221609"
 
-wget --quiet -O wpicpp.zip http://first.wpi.edu/FRC/roborio/release/eclipse/plugins/edu.wpi.first.wpilib.plugins.cpp_0.1.0.201501170231.jar 
-unzip wpicpp.zip resources/cpp.zip
-mkdir wpilib
-mv resources/cpp.zip ./
-rm -rf resources
-unzip -d wpilib/ cpp.zip
+source wpilib/version.txt
+if [ ! "$version" = "$downloaded_version" ] ; then 
+	wget --quiet -O wpicpp.zip http://first.wpi.edu/FRC/roborio/release/eclipse/plugins/edu.wpi.first.wpilib.plugins.cpp_$version.jar 
+	unzip wpicpp.zip resources/cpp.zip
+	mkdir wpilib
+	mv resources/cpp.zip ./
+	rm -rf resources
+	unzip -d wpilib/ cpp.zip
 
-rm -rf cpp.zip
-rm -rf wpicpp.zip
+	rm -rf cpp.zip
+	rm -rf wpicpp.zip
+	echo "downloaded_version=$version" > wpilib/version.txt
+else
+	echo "Already at latest version"
+fi
 
+echo "Version = $version"
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd build
-./build.sh
+cd ./.build
+rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=./arm.cmake robot.cmake ..
+make VERBOSE=1
 
