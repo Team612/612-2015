b35ddcd261ed4e6e91ddbb6b79c2734fe84c22a3
Alexbay218 <Alexbay218@gmail.com>
2015-02-12 18:26:32 -0500

    Added documentation

--- src/Commands/ElevatorMoveToPosition.h
+++ src/Commands/ElevatorMoveToPosition.h
@@ -9,10 +9,11 @@ class ElevatorMoveToPosition: public CommandBase
 private:
 	uint32_t targetPosition;
 	int targetSpins;
-	const int presets[4] = {1,2,3,4}; //filler value! TODO add real preset values!
+	const int presets[4] = {0,1,2,3,4}; //filler value! TODO add real preset values!
 	const uint32_t INTERVAL = 0; //Placeholder value TODO add real values!
 public:
-	ElevatorMoveToPosition(uint32_t position);
+	ElevatorMoveToPosition(uint32_t position); //example of use: ElevatorMoveToPosition(0);
+	//This would (hopefully) move the elevator to the first value in const int presets array, which is zero.
 	void Initialize();
 	void Execute();
 	bool IsFinished();
