19572a73ab09bd838abb22f1c29d8906650e34b8
DavidFiumano <Mynameisspam1@gmail.com>
2015-02-12 18:54:20 -0500

    Added OI stuff and drive stuff ;)

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -40,7 +40,8 @@ const uint32_t INFRARED = 1; //fix this number
 /*
  * Elevator
  */
-const uint32_t ELEVATOR_MOTOR = 5;
+const uint32_t ELEVATOR_MOTOR_1 = 5;
+const uint32_t ELEVATOR_MOTOR_2 = 6;
 const uint32_t ELEVATOR_TOP_SWITCH = 1;
 const uint32_t ELEVATOR_BOTTOM_SWITCH = 1;
 const uint32_t ELEVATOR_ENCODER_A = 1;
--- src/Subsystems/Elevator.cpp
+++ src/Subsystems/Elevator.cpp
@@ -3,7 +3,8 @@
 Elevator::Elevator() :
 	Subsystem("Elevator")
 {
-	talon = new CANTalon(ELEVATOR_MOTOR);
+	talon = new CANTalon(ELEVATOR_MOTOR_1);
+	talon2 = new CANTalon(ELEVATOR_MOTOR_2);
 	topSwitch = new DigitalInput(ELEVATOR_TOP_SWITCH);
 	bottomSwitch = new DigitalInput(ELEVATOR_BOTTOM_SWITCH);
 	encoder = new Encoder(ELEVATOR_ENCODER_A, ELEVATOR_ENCODER_B);
@@ -12,6 +13,7 @@ Elevator::Elevator() :
 Elevator::~Elevator()
 {
 	delete talon;
+	delete talon2;
 }
 
 void Elevator::InitDefaultCommand()
@@ -22,24 +24,27 @@ void Elevator::InitDefaultCommand()
 void Elevator::move(float magnitude)
 {
 	//Checks the sensors to see if the elevator is at the top or the bottom
-	bool topInput = topSwitch->Get();
-	bool bottomInput = bottomSwitch->Get();
+	//bool topInput = topSwitch->Get();
+	//bool bottomInput = bottomSwitch->Get();
 
 	//If the sensors give any input then the motors are set to a speed of 0
-	if (topInput || bottomInput)
-	{
-		talon->Set(0);
-	}
-	else
-	{
+	//if (topInput || bottomInput)
+	//{
+	//	talon->Set(0);
+	//	talon2->Set(0);
+	//}
+	//else
+	//{
 		talon->Set(magnitude);
-	}
+		talon2->Set(magnitude);
+	//}
 }
 
 void Elevator::stop()
 {
 	//Sets motor speed to nothing
 	talon->Set(0);
+	talon2->Set(0);
 }
 
 Encoder* Elevator::getEncoder()
