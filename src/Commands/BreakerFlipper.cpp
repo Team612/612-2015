beacda5d0cb21ceb290d7c5f72713b92a27737f6
Alexbay218 <Alexbay218@gmail.com>
2015-02-12 20:17:14 -0500

    Add stuff.
    Not done yet

---
+++ src/Commands/BreakerFlipper.cpp
@@ -0,0 +1,49 @@
+#include "BreakerFlipper.h"
+
+BreakerFlipper::BreakerFlipper()
+{
+	timer = new Timer();
+	encoder = new Encoder(channelCheck);
+	pid = new PIDSubsystem();
+}
+
+// Called just before this Command runs the first time
+void BreakerFlipper::Initialize()
+{
+
+}
+
+void BreakerFlipper::CheckMotor()
+{
+	if (encoder->GetRaw() <= 0)
+	{
+
+	}
+}
+// Called repeatedly when this Command is scheduled to run
+void BreakerFlipper::Execute()
+{
+	if(PowerBoard->GetCurrent(channelCheck) > ampLimit)
+	{
+		CheckMotor();
+	}
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool BreakerFlipper::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void BreakerFlipper::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void BreakerFlipper::Interrupted()
+{
+
+}
---
+++ src/Commands/BreakerFlipper.h
@@ -0,0 +1,30 @@
+#ifndef BreakerFlipper_H
+#define BreakerFlipper_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+#include "PowerBoard.h"
+
+class BreakerFlipper: public CommandBase
+{
+private:
+	Timer* timer;
+	Encoder* encoder;
+	PIDSubsystem* pid;
+
+	const double ampLimit = 0.0; //TODO Fill in the right value
+	const uint8_t channelCheck = 0; //TODO Fill in the right value
+	const uint32_t encoderChannelA = 0; //TODO Fill in the right value
+	const uint32_t encoderChannelB = 1; //TODO Fill in the right value
+
+public:
+	BreakerFlipper();
+	void Initialize();
+	void CheckMotor();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif
