a2a79c209e534c58c548fcb625f1ad4c2791d712
Benjamin Ward <ward.programm3r@gmail.com>
2015-03-09 20:01:41 -0400

    Adding base AutoPickup files

---
+++ src/Commands/AutoPickup.cpp
@@ -0,0 +1,10 @@
+/*
+ * AutoPickup.cpp
+ *
+ *  Created on: Mar 9, 2015
+ *      Author: user
+ */
+
+
+
+
---
+++ src/Commands/AutoPickup.h
@@ -0,0 +1,15 @@
+/*
+ * AutoPickup.h
+ *
+ *  Created on: Mar 9, 2015
+ *      Author: user
+ */
+
+#ifndef SRC_COMMANDS_AUTOPICKUP_H_
+#define SRC_COMMANDS_AUTOPICKUP_H_
+
+
+
+
+
+#endif /* SRC_COMMANDS_AUTOPICKUP_H_ */
