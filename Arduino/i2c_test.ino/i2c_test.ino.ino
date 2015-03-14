a9af29911c8e7b5356e33dfc9978e24c600d4188
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2015-03-14 13:55:10 -0400

    Arduino stuff

---
+++ Arduino/i2c_test.ino/i2c_test.ino.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(2);
+  Wire.onRequest(requestEvent);
+}
+
+void loop() {
+  delay(100);
+}
+
+void requestEvent() {
+  /*char* message[5]; //Hello
+  message[0] = "H";
+  message[1] = "E";
+  message[2] = "L";
+  message[3] = "L";
+  message[4] = "0";
+  for(int x; x<5; x){
+    Wire.write(message[x]);
+  }*/
+  Wire.write("hello");
+}
+
