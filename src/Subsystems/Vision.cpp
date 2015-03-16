5c211ccd4736d9da61d02936c6f48f56edde776c
Zach Perkins <thezperk@gmail.com>
2015-03-16 19:25:25 -0400

    Fixed memory leak in vision

--- src/Subsystems/Vision.cpp
+++ src/Subsystems/Vision.cpp
@@ -4,7 +4,6 @@
 Vision::Vision() :
 		Subsystem("Vision")
 {
-
 	i2c = new I2C(I2C::kOnboard, 2); //Initializes
 }
 
@@ -15,7 +14,10 @@ void Vision::InitDefaultCommand()
 
 void Vision::updateVision()
 {
-	uint8_t* data = new uint8_t[7];
+	static uint8_t* data = new uint8_t[7];
+	for (int i = 0; i < 7; i++)
+		data[i] = 0;
+
 	i2c->ReadOnly(5, data);
 	printf("I2C: %u\n", data[0]);
 }
