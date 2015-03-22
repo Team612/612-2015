27cc8b1f4eba3f41fa9386323b26a65d9cfe1522
Calvin Godfrey <strangeguy32@gmail.com>
2015-03-22 17:15:40 +0000

    Added parenthesis to algorithm so it returns reasonable numbers rather than 2.3426e-07. Before, it was dividing by two then multiplying by that tiny number making a SUPER small number, now it returns numbers between 17-300, much more reasonable

--- src/Commands/AngleAlgor.cpp
+++ src/Commands/AngleAlgor.cpp
@@ -21,11 +21,11 @@ double AngleAlgor(float wR, float hR, float wL,  float hL)
 
 	if(leftOrRight == 1) //Right side closer
 	{
-		return ( 0.0243029 + sqrt( ( -0.0243029 * -0.0243029 ) - ( 4 * 0.000111078 * (2.66466 - ratioAverage) ) ) ) / 2*0.000111078;
+		return ( 0.0243029 + sqrt( ( -0.0243029 * -0.0243029 ) - ( 4 * 0.000111078 * (2.66466 - ratioAverage) ) ) ) / (2*0.000111078);
 		//Line of best fit for the graph that returns the angle (fairly accurate)
 	}
 	else if(leftOrRight == -1)
 	{
-		return ( 0.00023557 + sqrt( ( -0.00023557 * -0.00023557 ) - ( 4 * 0.0000465902 * (2.2209 - ratioAverage) ) ) ) / 2*0.0000465902;
+		return ( 0.00023557 + sqrt( ( -0.00023557 * -0.00023557 ) - ( 4 * 0.0000465902 * (2.2209 - ratioAverage) ) ) ) / (2*0.0000465902);
 	}
 }
