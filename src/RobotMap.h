96bc55dee3c6ed300d6b08d426e279fadaa67b1c
ChampD <AMOTHWO@gmail.com>
2015-03-26 10:41:52 -0400

    changes at DC

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -59,7 +59,7 @@ const uint8_t REFRESH_RATE = 50;
 const float CRATE_HEIGHT = 12.0f;
 const float BUFFER       = 2.0f;
 
-const uint32_t ELEVATOR_ULTRASONIC = 1;
+const uint32_t ELEVATOR_ULTRASONIC = 0;
 const uint32_t ELEVATOR_IR = 2;
 
 const float MAX_IR    = 60.0f; //TODO
