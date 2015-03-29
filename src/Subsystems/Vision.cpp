268a81ba23dea5e653ecd747df37d7bca6cc4027
Zach Perkins <Thezperk@gmail.com>
2015-03-28 22:04:22 -0400

    Improved vision to do a better job with receiving information (Issue #154)

--- src/Subsystems/Vision.cpp
+++ src/Subsystems/Vision.cpp
@@ -5,8 +5,14 @@
 Vision::Vision() :
 		Subsystem("Vision")
 {
-	i2c = new I2C(I2C::kOnboard, 2);
-	Update();
+	wire = new I2C(I2C::kOnboard, 2);
+	flushData();
+}
+
+Vision::~Vision() {
+	delete wire;
+	delete cam0Data;
+	delete cam1Data;
 }
 
 void Vision::InitDefaultCommand()
@@ -14,11 +20,109 @@ void Vision::InitDefaultCommand()
 
 }
 
-void Vision::Update()
+
+void Vision::receiveI2C()
 {
 	static uint8_t* buffer = new uint8_t[7];
-	i2c->ReadOnly(7, buffer);
-	//CommandBase::pixellogic->I2CRecieve();
+	wire->ReadOnly(7, buffer);
+
+}
+
+void Vision::flushData()
+{
+	for (uint32_t i = 0; i < 3; i++)
+	{
+		cam0Data[i] = 0;
+		cam1Data[i] = 0;
+	}
+
+	cam0Received = false;
+	cam1Received = false;
+}
+
+uint16_t Vision::convertTo16Int(uint8_t* array, uint32_t startIndex)
+{
+	uint16_t result = array[startIndex] << 8 |
+					  array[startIndex + 1];
+
+	return result;
+}
+
+void Vision::transferData(uint8_t* buffer, uint16_t* target)
+{
+	int targetIndex = 0;
+
+	//Skip the byte used for the indicator
+	for (uint32_t i = 1; i < 7; i += 2)
+	{
+		target[targetIndex] = convertTo16Int(buffer, i);
+		targetIndex++;
+	}
+}
+
+void Vision::handleTransaction(uint8_t* buffer)
+{
+	uint8_t status = buffer[0];
+
+		switch(status) {
+		case 0:
+			cam0Received = true;
+			transferData(buffer, cam0Data);
+			break;
+		case 1:
+			cam1Received = true;
+			transferData(buffer, cam1Data);
+			break;
+		case 2:
+			cam0Received = false;
+			break;
+		case 3:
+			cam1Received = false;
+			break;
+		}
+}
+
+bool Vision::hasReceived(Camera camera)
+{
+	if (camera == LEFT)
+	{
+		return cam0Received;
+	}
+	else if (camera == RIGHT)
+	{
+		return cam1Received;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+uint16_t Vision::getRaw(Camera camera, uint32_t index) {
+	if (camera == LEFT)
+	{
+		return cam0Data[index];
+	}
+	else if (camera == RIGHT)
+	{
+		return cam1Data[index];
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+uint16_t Vision::getBoundingX(Camera camera) {
+	return getRaw(camera, 0);
+}
+
+uint16_t Vision::getBoundingWidth(Camera camera) {
+	return getRaw(camera, 1);
+}
+
+uint16_t Vision::getBoundingHeight(Camera camera) {
+	return getRaw(camera, 2);
 }
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
--- src/Subsystems/Vision.h
+++ src/Subsystems/Vision.h
@@ -5,17 +5,52 @@
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 #include "PixelLogic.h"
+#include <cmath>
 //#include "../CommandBase.h"
 
 class Vision: public Subsystem
 {
 private:
-	I2C *i2c;
+	I2C* wire;
+
+	//TODO: Not sure if I should change the variables names to 'left' and 'right' as opposed to '0' and '1'
+	uint8_t cam0Received = false;
+	uint8_t cam1Received = false;
+
+	uint16_t* cam0Data; //Stores all raw data from cameras
+	uint16_t* cam1Data;
+
+	uint16_t convertTo16Int(uint8_t* array, uint32_t beginIndex); //Converts to 16 bit unsigned int
+	void transferData(uint8_t* buffer, uint16_t* target); //Transfers data from 8-bit buffer to 16-bit target
+	void handleTransaction(uint8_t* buffer); //Handles all readings from the i2c
 
 public:
+	//Easier way of accessing cameras
+	enum Camera {LEFT, RIGHT};
+
 	Vision();
 	void InitDefaultCommand();
-	void Update();
+
+	//Needs to be called by the routine using vision every tick, receives the new info
+	void receiveI2C();
+
+	//Removes all data in the camera's caches
+	void flushData();
+
+	//Check to see if the camera has received information on it's last ping
+	bool hasReceived(Camera camera);
+
+	//Gets an index of a cameras raw data
+	uint16_t getRaw(Camera camera, uint32_t index);
+
+	//Retrieves the X position of a camera's bounding box
+	uint16_t getBoundingX(Camera camera);
+
+	//Retrieves the width of a camera's bounding box
+	uint16_t getBoundingWidth(Camera camera);
+
+	//Retrieves the height of a camera's bounding box
+	uint16_t getBoundingHeight(Camera camera);
 };
 
 #endif
