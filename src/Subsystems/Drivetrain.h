d16160f02daead22ad89715c2680bb2da0199810
Calvin Godfrey <strangeguy32@gmail.com>
2015-05-30 01:06:50 +0000

    Added documentation to elevator, drivetrain, testmotors, and roughalign

--- src/Subsystems/Drivetrain.h
+++ src/Subsystems/Drivetrain.h
@@ -14,7 +14,7 @@ class Drivetrain: public Subsystem, public RobotDrive
 private:
 	//bool useIR = false;
 
-	AnalogInput* ir;
+	AnalogInput* ir; ///< Infrared object
 	//Ultrasonic* ultra;
 	//AnalogInput* infrared;
 
@@ -23,33 +23,43 @@ private:
 	//Encoder* encoderRR;
 	//Encoder* encoderRF;
 
-	bool useIR = false;
+	bool useIR = false; ///<Bool if using IR to detect distance
 	//bool SwitchSensor(float distance); // Called in CheckSensor
 
 public:
+	///Constructor for drivetrain
+	///@param fl front left talon
+	///@param fr front right talon
+	///@param rf rear front talon
+	///@param rr rear right talon
+	///@param infrared Object used to detect distance
 	Drivetrain(CANTalon* fl, CANTalon* fr, CANTalon* rf,CANTalon* rr, AnalogInput* infrared);
-	void InitDefaultCommand();
+	void InitDefaultCommand(); ///< Inits default command for drivetrain
 
 	//void CheckSensor(float distance);
+	///Moves drivetrain
+	///@param x distance to move left/right (inches? Encoder measurement?)
+	///@param y distance to move forward/back (inches? Encoder?)
+	///@param rotation degrees to rotate (NOT RADIANS)
 	void move(float x, float y, float rotation);
-	void stop();
-	int16_t getir();
+	void stop();///< Stops robot
+	int16_t getir(); ///<Returns IR sensor object
 
 	//Encoder Methods
-	enum MotorLocation {LEFT_FRONT, LEFT_REAR, RIGHT_FRONT, RIGHT_REAR}; //For use in getDistance
+	enum MotorLocation {LEFT_FRONT, LEFT_REAR, RIGHT_FRONT, RIGHT_REAR}; ///<For use in getDistance
 	//void resetEncoders(); //Resets ALL encoders
-	int32_t getDistance(MotorLocation location); //Gets the distance of a specific  motor
+	int32_t getDistance(MotorLocation location); ///<Gets the distance of a specific motor
 	//void SetRightFeedWheel(float speed);
 	//void SetLeftFeedWheel(float speed);
 
-	CANTalon* fl;
-	CANTalon* rl;
-	CANTalon* fr;
-	CANTalon* rr;
+	CANTalon* fl; ///< front left talon object
+	CANTalon* rl; ///< rear left talon object
+	CANTalon* fr; ///< front right talon object
+	CANTalon* rr; ///< rear right talon object
 
-	float motor_power;
+	float motor_power; ///< power of motor (0-1)
 
-	IMU* imu;
+	IMU* imu; ///<Object to detect intertia (I think?)
 };
 
 #endif
--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -8,41 +8,55 @@
 
 class Elevator : public Subsystem {
 private:
-	CANTalon* talon;
+	CANTalon* talon; ///< First talon object
 #ifdef TALON
-	CANTalon* talon2;
+	CANTalon* talon2; ///< If the robot got it, second talon object
 #endif
 	//DigitalInput* topSwitch;
 	//DigitalInput* bottomSwitch;
-	Encoder* encoder;
+	Encoder* encoder; ///< Encoder object (on talon to measure distance)
 	//AnalogInput* leftIR;
 	//AnalogInput* rightIR;
+	/// Converts voltage to distance
+	/// Used to convert voltage from the IR sensor to distance
+	/// Never 100% sure this function ever worked
+	///@param val voltage from the IR sensor
 	float voltageToDistance(float val);
+	/// Which sensor to be used by the robot
+	/// Ultrasonic and Infrared have different distances that they are accurate for
 	typedef enum _MainSensor
 	{
-		ULTRASONIC, 
-		IR
+		ULTRASONIC, ///< Ultrasonic being used to detect distance
+		IR ///< infrared being used to dectect
 	} MainSensor;
-	MainSensor sense;
-	const float SWITCH_TO_ULTRASONIC = 12.0f;
-	const float SWITCH_TO_IR = 6.0f;
-	AnalogInput* ultrasonic;
-	AnalogInput* elevatorIR;
+	MainSensor sense; ///<Variable used to store which sensor being used
+	const float SWITCH_TO_ULTRASONIC = 12.0f; ///< Temp value for when to switch from IR to US
+	const float SWITCH_TO_IR = 6.0f; ///< Temp value to switch from US to IR
+	AnalogInput* ultrasonic; ///< Ultrasonic object
+	AnalogInput* elevatorIR; ///< Infrared Object (used on elevator)
+	///Switches sensor if necessary
+	///If value of one falls outside of accurate range of other, switch
+	///Default set to IR
+	///@param IRDistance distance detected by IR
+	///@param UDistance distance detected by ultrasonic
 	MainSensor switchSensor(float IRDistance, float UDistance);
-	DoubleSolenoid* latchSol;
+	DoubleSolenoid* latchSol; ///<Double solenoid object (for latch)
 public:
-	Elevator();
-	virtual ~Elevator();
+	Elevator(); ///<Elevator constructor
+	virtual ~Elevator(); ///< Elevator deconstructor
+	///Moves elevator
+	///Moves elevator at a given speed
+	///@param speed float that says what speed to move elevator at
 	void move(float speed);
-	void stop();
-	void InitDefaultCommand();
-	Encoder* getEncoder();
-	bool getLeftAlignment();
-	bool getRightAlignment();
-	float getElevatorHeight();
-	float UltrasonicVoltageToDistance(float val);
-	float IRVoltageToDistance(float val);
-	DoubleSolenoid* getSolenoid();
+	void stop(); ///< Stops elevator (incase of tipping)
+	void InitDefaultCommand(); ///< Starts default command
+	Encoder* getEncoder(); ///< Function that returns the encoder
+	bool getLeftAlignment(); ///< Returns if left side of robot is aligned with crate
+	bool getRightAlignment(); ///< Returns if right side of robot is aligned with crate
+	float getElevatorHeight(); ///< Gets height of elevator from ground
+	float UltrasonicVoltageToDistance(float val); ///<Converts voltage to distance (US)
+	float IRVoltageToDistance(float val); ///< Converts voltage to distance (IR)
+	DoubleSolenoid* getSolenoid(); ///< Returns solenoid
 };
 
 #endif /* LATCH_CPP_SRC_SUBSYSTEMS_ELEVATOR_H_ */
